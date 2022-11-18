cd015e8566f8ac21363a9aec69086ad853ede339
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-11-18 14:16:34 +0100

    Add: API function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import './style.css';
-import renderScores from './modules/render.js';
-import addScore from './modules/add-score.js';
 
-const addScoreBtn = document.querySelector('.submit');
-addScoreBtn.addEventListener('click', (e) => {
+// import renderScores from './modules/render.js';
+// import addScore from './modules/add-score.js';
+
+// const addScoreBtn = document.querySelector('.submit');
+// addScoreBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   addScore();
+// });
+
+const gameId = 'XWZaQZBPVullch0IYVvl';
+const list = document.querySelector('.score-list');
+
+const render = (data) => {
+  const li = document.createElement('li');
+  li.innerHTML = `${data.user} : ${data.score}`;
+  li.className = 'list-item';
+  list.appendChild(li);
+};
+
+async function postData(data) {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+async function fetchData() {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, {
+    method: 'GET',
+  });
+  return response.json();
+}
+
+document.querySelector('form').onsubmit = (e) => {
   e.preventDefault();
-  addScore();
+  const name = e.target.name.value;
+  const score = e.target.score.value;
+  if (score === '' || name === '') {
+    alert('Please fill in both fields');
+  } else if (Number.isNaN(Number(score))) {
+    alert('Please enter a number');
+  } else {
+    postData({ user: name, score })
+      .then((data) => {
+        if (data) {
+          render({ user: name, score });
+          e.target.name.value = '';
+          e.target.score.value = '';
+        }
+      });
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchData()
+    .then((data) => {
+      data.result.forEach((item) => { render(item); });
+    });
 });
 
-const refreshBtn = document.querySelector('.refresh');
-refreshBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  window.location.reload();
-});
-
-renderScores();
+document.querySelector('.refresh').onclick = () => {
+  list.innerHTML = '';
+  fetchData()
+    .then((data) => {
+      data.result.forEach((item) => { render(item); });
+    });
+};
